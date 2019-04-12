be76ee449a2e42d5f9c67979eac72ef42fd9c5f8
mhashim6 <m6hashim@gmail.com>
2019-04-13 00:04:39 +0200

    traps fixes and refactoring.

--- traps.py
+++ traps.py
@@ -1,7 +1,7 @@
 from memory import mem_read, mem_write, Registers, reg_read, reg_write
 from utils import sign_extend
 from enum import Enum
-from platform_getch import getch
+from getch import getch
 import sys
 
 
@@ -18,46 +18,32 @@ def _GETC():
 
 def _OUT():
     """output a character"""
-    sys.stdout.write(chr(mem_read(reg_read(Registers.R0))))
+    sys.stdout.write(chr(reg_read(Registers.R0)))
     sys.stdout.flush()
 
 
 def _PUTS():
     """output a word string"""
-    i = reg_read(Registers.R0)
-    ch = mem_read(i)
-    while chr(ch) != '\0':  # check if the char is not null then print this char
-        sys.stdout.write(ch)
-        i += 1
+    for i in range(reg_read(Registers.R0), 2**16):
         ch = mem_read(i)
+        if ch == 0:  # check if the char is not null then print this char
+            break
+        sys.stdout.write(chr(ch))
     sys.stdout.flush()  # equal to fflush() in c
 
 
 def _IN():
     """input a string"""
-    # get string from user
-    ch_arr = map(ord, list(input()))
-    # get memory location to write data in
-    i = reg_read(Registers.R0)
-    for ch in ch_arr:
-        if chr(ch) != '\0':
-            mem_write(i, ch)
-            i += 1
-        else:
-            break
-
-    """ch = _GETC()
-    i = reg_read(Registers.R0)
-    while chr(ch) != '\0':
-        mem_write(i, ch)
-        i += 1"""
+    sys.stdout.write("Enter a character: ")
+    sys.stdout.flush()
+    reg_write(Registers.R0, sys.stdout.read(1))
 
 
 def _PUTSP():
     """output a byte string"""
-    for i in range(reg_read(Registers.R0), (2**16)):
+    for i in range(reg_read(Registers.R0), 2**16):
         c = mem_read(i)
-        if chr(c) == '\0':
+        if c == 0:
             break
         sys.stdout.write(chr(c & 0xFF))
         char = c >> 8
@@ -72,21 +58,21 @@ def _HALT():
 
 
 class Traps(Enum):
-    TRAP_GETC = 0x20    # get character from keyboard
-    TRAP_OUT = 0x21     # output a character
-    TRAP_PUTS = 0x22    # output a word string
-    TRAP_IN = 0x23      # input a string
-    TRAP_PUTSP = 0x24   # output a byte string
-    TRAP_HALT = 0x25    # halt the program
+    GETC = 0x20    # get character from keyboard
+    OUT = 0x21     # output a character
+    PUTS = 0x22    # output a word string
+    IN = 0x23      # input a string
+    PUTSP = 0x24   # output a byte string
+    HALT = 0x25    # halt the program
 
 
 _traps = {
-    Traps.TRAP_GETC: _GETC,
-    Traps.TRAP_OUT: _OUT,
-    Traps.TRAP_PUTS: _PUTS,
-    Traps.TRAP_IN: _IN,
-    Traps.TRAP_PUTSP: _PUTSP,
-    Traps.TRAP_HALT: _HALT
+    Traps.GETC: _GETC,
+    Traps.OUT: _OUT,
+    Traps.PUTS: _PUTS,
+    Traps.IN: _IN,
+    Traps.PUTSP: _PUTSP,
+    Traps.HALT: _HALT
 }
 
 
