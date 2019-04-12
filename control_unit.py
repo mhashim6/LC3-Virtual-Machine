12e70b9375276a70fb5445d783a1f3c1ab59649a
Muhammad Hashim <m6hashim@gmail.com>
2019-04-12 17:56:53 +0200

    removed unnecessary parentheses.

--- control_unit.py
+++ control_unit.py
@@ -73,7 +73,7 @@ def _RTI(instruction):
 
 def _NOT(instruction):
     DR = (instruction >> 9) & 0X7                                # Destination Register.
-    SR = ((instruction) >> 6) & 0X7                                # Source Register (register cntaining the data).
+    SR = (instruction >> 6) & 0X7                                # Source Register (register cntaining the data).
     reg_write(Registers(DR), ~reg_read(Registers(SR)))             # every bit in the DR will equal to the flipped one with the same index in SR. 
     update_flags(DR)                                               # store the sign of the last excuted instruction data (which is in the DR).
     
